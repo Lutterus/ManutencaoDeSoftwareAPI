8b6b9d0aa60ecefd1ad03cd0f62efb4d01a490c6
Vieira <Wendell_Vieira@Dell.com>
2018-08-24 21:34:51 -0300

    Adicionado teste de conexao no banco

--- source/app.js
+++ source/app.js
@@ -6,7 +6,25 @@ const PORT = process.env.PORT || 8000
 
 
 app.get('/', (req, res) => {
-  res.send('Everything seems fine here')
+ // res.send('Everything seems fine here')
+
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'test',
+  password : 'sDeX695SpDGpLXWq',
+  database : 'milhas'
+});
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+});
+
+connection.end();
 })
 
 app.get('/api/getPrograms/:userId', routes.getPrograms)
@@ -14,3 +32,4 @@ app.get('/api/getPrograms/:userId', routes.getPrograms)
 app.listen(PORT, () => {
   console.log(`Started application at port: ${PORT}`)
 })
+
