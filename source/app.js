78e513770ab6711bfcf684b5d9f184b3358381c6
andrei.oliveira <andrei.oliveira@acad.pucrs.br>
2018-08-27 19:50:00 -0300

    config database with sequelize

--- source/app.js
+++ source/app.js
@@ -1,47 +1,40 @@
 const express = require('express')
 const routes = require('./routes/routes.js')
-
+const { PORT } = require('../settings.js')
+const DB = require('./database.js')
+const connection = DB.connect()
 
 const app = express()
-const PORT = process.env.PORT || 8024
-
-const qs = require('querystring');
-
-var resultjason = [];
-	
-app.get('/', (req, res) => {
-  res.send('Everything seems fine here')
 
 
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'test',
-  password : 'sDeX695SpDGpLXWq',
-  database : 'milhas'
-});
-;
-
-connection.connect();
-
-  connection.query("SELECT * FROM conta", function (err, result, fields) {
-    if (err) throw err;
-	Object.keys(result).forEach(function(key) {
-
-    var format = qs.parse(`login=${result[key].login};senha=${result[key].senha};cod_usario=${result[key].cod_usario};`, ';', '=')
-    
-    console.log(format)
-    });
-   
+connection
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
   });
+const Programa = require('./models/index.js')(connection)
 
+const startApp = () => {
+  app.get('/', (req, res) => {
+    Programa.findOne({})
+      .then(data => {
+        res.send('DATA: '+data)
+      })
+    
+  })
+  
+  app.get('/api/getPrograms/:userId', routes.getPrograms)
+  
+  app.listen(PORT, () => {
+    console.log(`Started application at port: ${PORT}`)
+  })
+  
+  
+}
 
-connection.end();
-})
-
-app.get('/api/getPrograms/:userId', routes.getPrograms)
-
-app.listen(PORT, () => {
-  console.log(`Started application at port: ${PORT}`)
-})
-
+connection.sync()
+  .then(startApp())
+  .catch(err => console.log(err))
