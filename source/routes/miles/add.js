c5be6d1de785a6842c7a2e747d0164fdda730380
Leonardo Veiga <leonardo.freitas.veiga@gmail.com>
2018-09-21 18:59:38 -0300

    Arrumando erro ao cadastrar novas milhas

--- source/routes/miles/add.js
+++ source/routes/miles/add.js
@@ -1,13 +1,5 @@
 const addMiles = (Milha, Programa) => (req, res, next) => {
 
-	console.log('POST /');
-	console.log(req.body);
-	console.log(req.body.program);
-
-	//Programa.findById(req.body.program).then(program => {
-  		// project will be an instance of Project and stores the content of the table entry
-  		// with id 123. if such an entry is not defined you will get null
-
   	Programa.findOne({ where: {
   			nome: req.body.program,
   			contaLogin: req.body.user,
@@ -15,56 +7,60 @@ const addMiles = (Milha, Programa) => (req, res, next) => {
   		} }).then(program => {
   		// project will be the first entry of the Projects table with the title 'aProject' || null
 
-  		let codProgram;
-  		console.log(program);
   		if(program == null) {
   			console.log("[addMiles] Programa não existe, criando novo programa para usuário!")
   			Programa.max('cod_programa').then(max => {
 			 	Programa.create({
 		  			cod_programa: max+1,
 		  			nome: req.body.program,
-		  			somaMilhas: req.body.milhas,
-		  			milha_expiracao_maisProxima: req.body.closestExpiration
+		  			somaMilhas: req.body.miles,
+		  			milha_expiracao_maisProxima: null,
+		  			contaLogin: req.body.user
 		  		}).then(createdProgram => {
-		  			codProgram = createdProgram.cod_programa
+
+		  			Milha.max('cod_milha').then(max => {
+					  	Milha.create({ 
+							cod_milha: max+1, 
+							quantidade: req.body.miles, 
+							dt_expiracao: (req.body.expirationDate ? req.body.expirationDate : new Date()),
+							cod_programa: createdProgram.dataValues.cod_programa,
+							contaLogin: createdProgram.dataValues.contaLogin
+						})
+						.then(mile => {
+						  	console.log('[addMiles] SUCESS!')
+						  	res.writeHead(201, {'Content-Type': 'text/html'});
+							res.end('CREATED');
+						})
+					})
+
 		  		})
 			})
 	  	} else {
 	  		console.log("[addMiles] Programa encontrado!")
 	  		codProgram = program.cod_programa
-	  	}
 
-		Milha.max('cod_milha').then(max => {
-		  	Milha.create({ 
-				cod_milha: max+1, 
-				quantidade: req.body.miles, 
-				dt_expiracao: (req.body.expirationDate ? req.body.expirationDate : new Date()),
-				cod_programa: codProgram,
-				contaLogin: req.body.user
+	  		Milha.max('cod_milha').then(max => {
+			  	Milha.create({ 
+					cod_milha: max+1, 
+					quantidade: req.body.miles, 
+					dt_expiracao: (req.body.expirationDate ? req.body.expirationDate : new Date()),
+					cod_programa: program.cod_programa,
+					contaLogin: req.body.user
+				})
+				.then(mile => {
+				  	console.log('[addMiles] SUCESS!')
+				  	res.writeHead(201, {'Content-Type': 'text/html'});
+					res.end('CREATED');
+				})
 			})
-			.then(mile => {
-			  	console.log('SUCESS!')
-			  	res.writeHead(201, {'Content-Type': 'text/html'});
-				res.end('CREATED');
-			})
-		})
+
+	  	}
   		
 	})
 	.catch(err => {
-      	console.error(`[ERROR] ${JSON.stringify(err)}`)
+      	console.error(`[addMiles][ERROR] ${JSON.stringify(err)}`)
       	next({ status: 500, message: "internal_server_error" })
     })
-
-	/*
-  Milha.findAll({ where: {contaLogin: 'ciclano', cod_programa: '21'} })
-    .then(milhas => {
-      res.send(milhas)
-    })
-    .catch(err => {
-      console.error(`[ERROR] ${JSON.stringify(err)}`)
-      next({ status: 500, message: "internal_server_error" })
-    })
-    */
 }
 
 module.exports = addMiles
