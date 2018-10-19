2c669b33516ecd6b9a6c54b85432e55f7b904bf4
Juan Collin <juanlucas09@hotmail.com>
2018-10-19 20:50:59 -0300

    Corrigido erro de quando não tinha milhas

--- source/routes/programs/all.get.js
+++ source/routes/programs/all.get.js
@@ -12,27 +12,29 @@ const getPrograms = (Programa, ProgramaDefault, Milha) => (req, res, next) => {
     .then(programas => {
       var listaFinal = []
       var contPrograma = 0;
-      programas.forEach(function(programa) {
-        Milha.findOne({ where: {
-          cod_milha: programa.dataValues.milha_expiracao_maisProxima
-          
-        } }).then(milha => {
-          let novoPrograma = {
-            cod_programa: programa.dataValues.cod_programa,
-            nome: programa.dataValues.nome,
-            somaMilhas: programa.dataValues.somaMilhas,
-            milha_expiracao_maisProxima: milha.dataValues,
-            contaLogin: programa.dataValues.contaLogin,
-            contaSenha: programa.dataValues.contaSenha,
-            programa_default: programa.dataValues.programa_default
-          }
-          listaFinal.push(novoPrograma)
-          contPrograma++
-          if(contPrograma === programas.length) {
-            res.send(listaFinal)
-          }
-        })
-      });
+      if (programas != null) {
+        programas.forEach(function(programa) {
+          Milha.findOne({ where: {
+            cod_milha: programa.dataValues.milha_expiracao_maisProxima
+            
+          } }).then(milha => {
+            let novoPrograma = {
+              cod_programa: programa.dataValues.cod_programa,
+              nome: programa.dataValues.nome,
+              somaMilhas: programa.dataValues.somaMilhas,
+              milha_expiracao_maisProxima: milha.dataValues,
+              contaLogin: programa.dataValues.contaLogin,
+              contaSenha: programa.dataValues.contaSenha,
+              programa_default: programa.dataValues.programa_default
+            }
+            listaFinal.push(novoPrograma)
+            contPrograma++
+            if(contPrograma === programas.length) {
+              res.send(listaFinal)
+            }
+          })
+        });
+      }  
     })
     .catch(err => {
       console.error(`[ERROR] ${JSON.stringify(err)}`)
